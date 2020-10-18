29425a7945dbff3fbe4b147df8e54c2fa0567c8f
Sergio <sergutpal@hotmail.com>
2020-10-18 02:30:02 +0200

    18102020 0230

--- raspiWeb/backoffice/tgScripts/energia.py
+++ raspiWeb/backoffice/tgScripts/energia.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python
 
 import globalVars
+import time
 
 globalVars.getValues(globalVars.pathEfergyDB, 'ENERGIA', 'efergy',
                      'historicoefergy', 'energia', 'W', globalVars.sendFile)
+txtOut = "PVNow: " + str(globalVars.redisGet(globalVars.redisPVNow))
+txtOut = txtOut + "W. PVSumToday: " + str(globalVars.redisGet(globalVars.redisPVSumToday)) + "KW"
+time.sleep(0.5)
+globalVars.toFile(globalVars.sendFile, txtOut)
