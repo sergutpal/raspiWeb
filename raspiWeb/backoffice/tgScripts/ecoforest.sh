ace896b4ce0a618b638d9698fabc165d03626254
Sergio <sergutpal@hotmail.com>
2021-01-07 21:10:34 +0100

    07012021 2110

--- raspiWeb/backoffice/tgScripts/ecoforest.sh
+++ raspiWeb/backoffice/tgScripts/ecoforest.sh
@@ -1,26 +1,16 @@
 #!/bin/bash
 
-for ((i=0; i<10; i++)) do
+for ((i=0; i<14; i++)) do
 
   if [[ $((i % 2)) -eq 0 ]];
   then
-    ENTITY="switch.ecoforestsalon"
+    curl -X POST -H "Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJiZDMxNTVkOGQ5OWY0ZjIwODUzOWEzZmI0NzNjNWY0MCIsImlhdCI6MTYwOTg3OTc0MSwiZXhwIjoxOTI1MjM5NzQxfQ.Yrg9Urw7t81xc07b-O3FEBF2QgeDfeAmthnR0wOMSzo" \
+      -H "Content-Type: application/json" -d '{"entity_id": "switch.ecoforestsalon"}' https://ha.sergutpal.dynu.com/api/services/switch/turn_on
   else
-    ENTITY="switch.ecoforestsalon2"
+    curl -X POST -H "Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJiZDMxNTVkOGQ5OWY0ZjIwODUzOWEzZmI0NzNjNWY0MCIsImlhdCI6MTYwOTg3OTc0MSwiZXhwIjoxOTI1MjM5NzQxfQ.Yrg9Urw7t81xc07b-O3FEBF2QgeDfeAmthnR0wOMSzo" \
+      -H "Content-Type: application/json" -d '{"entity_id": "switch.ecoforestsalon2"}' https://ha.sergutpal.dynu.com/api/services/switch/turn_on
   fi
-  JSON='{"entity_id": '$ENTITY'}'
-  JSON="'$JSON'"
-  echo $JSON
-
-  TOKEN="Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJiZDMxNTVkOGQ5OWY0ZjIwODUzOWEzZmI0NzNjNWY0MCIsImlhdCI6MTYwOTg3OTc0MSwiZXhwIjoxOTI1MjM5NzQxfQ.Yrg9Urw7t81xc07b-O3FEBF2QgeDfeAmthnR0wOMSzo"
-  TOKEN="'$TOKEN'"
-  CTYPE="Content-Type: application/json"
-  CTYPE="'$CTYPE'"
-  URL="https://ha.sergutpal.dynu.com/api/services/switch/turn_on"
-  CMD="curl -X POST -H $TOKEN -H $CTYPE -d $JSON $URL"
-  echo $CMD
-  eval "$CMD";
-  sleep 1.5
+  sleep 1.2
 done
 
 
